b654f7ee4cf26b9d6f0e161863a0963fcf242a9d
YtaloWill <ytalowillgloria@gmail.com>
2020-08-20 10:49:50 -0300

    just hello world

---
+++ MA4_20_08_PrimeiroPrograma/src/ma4_20_08_primeiroprograma/MA4_20_08_PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma4_20_08_primeiroprograma;
+
+/**
+ *
+ * @author ytalo
+ */
+public class MA4_20_08_PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
